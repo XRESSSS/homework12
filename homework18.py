d8c72d0335c6e6dfb1f3dd6657c3d55c59400e09
evgen <evgenijfucenko2@gmail.com>
2023-09-29 17:28:13 +0300

    oke, that's already work

--- homework18.py
+++ homework18.py
@@ -1,5 +1,5 @@
 import csv
-import time
+
 
 class Car:
     car_instances = []
@@ -10,6 +10,9 @@ class Car:
         self.year_of_release = year_of_release
         self.fuel_consumption = fuel_consumption
         self.run = 0
+        with open("car_info.csv", newline='', mode="a") as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerow([self.brand, self.producer, "scrapped"])
         Car.car_instances.append(self)
 
     def __str__(self):
@@ -17,23 +20,20 @@ class Car:
 
     __repr__ = __str__
 
-    def __del__(self):
-        Car.car_instances.remove(self)
-        with open("car_info.csv", "a") as csv_file:
-            csv_file.write('dsadsad')
-            # writer = csv.writer(csv_file)
-            # writer.writerow([self.brand, self.producer, "scrapped"])
-
 class Lorry(Car):
-    def __init__(self, producer, brand, year_of_release=2020, fuel_consumption: float = 0.0, cargo_capacity: float = 0.0):
+    def __init__(self, producer, brand, year_of_release=2020, fuel_consumption: float = 0.0,
+                 cargo_capacity: float = 0.0):
         super().__init__(producer, brand, year_of_release, fuel_consumption)
         self.cargo_capacity = cargo_capacity
 
+
 class SportsCar(Car):
-    def __init__(self, producer, brand, year_of_release=2020, fuel_consumption: float = 0.0, max_speed: float = 0.0, price: float = 0.0):
+    def __init__(self, producer, brand, year_of_release=2020, fuel_consumption: float = 0.0, max_speed: float = 0.0,
+                 price: float = 0.0):
         super().__init__(producer, brand, year_of_release, fuel_consumption)
         self.max_speed = max_speed
         self.price = price
+
 
 car = Car("Toyota", "Camry", 2022, 7.5)
 car2 = Car("Honda", "Civic", 2021, 6.8)
@@ -43,15 +43,4 @@ car5 = Car("Volkswagen", "Bugatti Automobiles", 2015, 10.1)
 
 lorry = Lorry("Ford", "Transit", 2021, 9.0, 2500)
 sports_car = SportsCar("Ferrari", "458 Italia", 2020, 12.5, 205, 250000)
-
-# Deleting the instances
-del car
-del car2
-del car3
-del car4
-del car5
-del lorry
-del sports_car
-time.sleep(2)
-print('dsad')
 
